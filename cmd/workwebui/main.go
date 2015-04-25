f903228ec2413a752cff776a203d39e497dc6400
Jonathan Novak <jnovak@gmail.com>
2015-04-24 20:50:13 -0700

    Make the webui respond to signals

--- cmd/workwebui/main.go
+++ cmd/workwebui/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
-	"time"
-	"github.com/gocraft/work/webui"
 	"github.com/garyburd/redigo/redis"
+	"github.com/gocraft/work/webui"
+	"os"
+	"os/signal"
+	"time"
 )
 
 var redisHostPort = flag.String("redis", ":6379", "redis hostport")
@@ -14,20 +16,27 @@ var webHostPort = flag.String("listen", ":5040", "hostport to listen for HTTP JS
 
 func main() {
 	flag.Parse()
-	
+
 	fmt.Println("Starting workwebui:")
 	fmt.Println("redis = ", *redisHostPort)
 	fmt.Println("namespace = ", *redisNamespace)
 	fmt.Println("listen = ", *webHostPort)
-	
+
 	pool := newPool(*redisHostPort)
-	
+
 	server := webui.NewServer(*redisNamespace, pool, *webHostPort)
 	server.Start()
-	
-	select{}
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, os.Kill)
+
+	<-c
+
+	server.Stop()
+
+	fmt.Println("\nQuitting...")
 }
- 
+
 func newPool(addr string) *redis.Pool {
 	return &redis.Pool{
 		MaxActive:   3,
