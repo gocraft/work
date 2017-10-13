56a4b49cae7335a6eb8c7ca5ba858f71deb61307
Andrei Milas <andreimilas@gmail.com>
2017-10-13 13:45:41 +0300

    Feature - add parameters to periodic jobs

--- worker_pool.go
+++ worker_pool.go
@@ -158,13 +158,13 @@ func (wp *WorkerPool) JobWithOptions(name string, jobOpts JobOptions, fn interfa
 // The spec format is based on https://godoc.org/github.com/robfig/cron, which is a relatively standard cron format.
 // Note that the first value is the seconds!
 // If you have multiple worker pools on different machines, they'll all coordinate and only enqueue your job once.
-func (wp *WorkerPool) PeriodicallyEnqueue(spec string, jobName string) *WorkerPool {
+func (wp *WorkerPool) PeriodicallyEnqueue(spec string, jobName string, args map[string]interface{}) *WorkerPool {
 	schedule, err := cron.Parse(spec)
 	if err != nil {
 		panic(err)
 	}
 
-	wp.periodicJobs = append(wp.periodicJobs, &periodicJob{jobName: jobName, spec: spec, schedule: schedule})
+	wp.periodicJobs = append(wp.periodicJobs, &periodicJob{jobName: jobName, spec: spec, schedule: schedule, args: args})
 
 	return wp
 }
