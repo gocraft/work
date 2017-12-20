615504e8d414090b342e1c817370d380894cf469
Pan Xiaohuai <panxiaohuai@guazi.com>
2017-12-20 16:11:54 +0800

    guazi library

---
+++ task/metrics.go
@@ -0,0 +1,138 @@
+package task
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/Guazi-inc/go-work"
+	"github.com/go-errors/errors"
+)
+
+//redis keys
+const (
+	keyTasks        = "__all_tasks__"
+	keyLock         = keyTasks + ":__lock_report__"
+	hashKeySucc     = "success"
+	hashKeyFail     = "fail"
+	hashKeyElapsed  = "elapsed"
+	hashKeyErr      = "last_error"
+	hashKeyDesc     = "desc"
+	hashKeyOwner    = "owner"
+	hashKeySchedule = "schedule"
+)
+
+// ServicePrefix for LockKey name, should unique global, will be override when create worker_pool_manager
+var ServicePrefix = ""
+
+type jobMetrics struct {
+	name     string
+	ns       string
+	owner    string
+	schedule string
+	succ     string
+	fail     string
+	elapsed  string
+	lastErr  string
+	desc     string
+}
+
+// saveMetrics called after exec the job whenever fail or success
+func saveMetrics(t *TaskBase, j *work.Job, elapsed time.Duration, runErr error) {
+	if redis == nil {
+		return
+	}
+
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println("[go-work error] saver Metrics err: ", err)
+		}
+	}()
+
+	if ok, _ := redis.HSetNX(keyTask(), fieldKey(t, j, hashKeyOwner), t.Owner); ok {
+		_ = redis.HSet(keyTask(), fieldKey(t, j, hashKeyDesc), t.GetDesc())
+		_ = redis.HSet(keyTask(), fieldKey(t, j, hashKeySchedule), t.GetSpecStr(j.Name))
+	}
+
+	if runErr == nil {
+		_, _ = redis.HIncrBy(keyTask(), fieldKey(t, j, hashKeyElapsed), elapsed.Nanoseconds()/int64(time.Millisecond))
+		_ = redis.HIncr(keyTask(), fieldKey(t, j, hashKeySucc))
+	}
+
+}
+
+func fetchMetrics() (map[string]*jobMetrics, error) {
+	if redis == nil {
+		return nil, errors.New("[go-work error] redis cache nil!")
+	}
+	// duplicate sending check, if prefix not specified, will skip lock check
+	if ServicePrefix != "" {
+		if rl, err := redis.SetNX(ServicePrefix+keyLock, fmt.Sprintf("%d", time.Now().UnixNano()/1e6), time.Hour*23); err == nil && !rl {
+			return nil, nil
+		}
+	}
+
+	res, err := redis.HGetAll(keyTask())
+	if err != nil {
+		return nil, err
+	}
+	_ = redis.Del(keyTask())
+
+	jobs := make(map[string]*jobMetrics)
+	for k, v := range res {
+		l := strings.Split(k, ":")
+		key := l[0] + l[1]
+		js, ok := jobs[key]
+		if !ok {
+			js = &jobMetrics{
+				name: l[1],
+				ns:   l[0],
+			}
+		}
+
+		switch l[2] {
+		case hashKeySucc:
+			js.succ = v
+		case hashKeySchedule:
+			js.schedule = v
+		case hashKeyFail:
+			js.fail = v
+		case hashKeyOwner:
+			js.owner = v
+		case hashKeyElapsed:
+			js.elapsed = v
+		case hashKeyDesc:
+			js.desc = v
+		case hashKeyErr:
+			js.lastErr = v
+		}
+		jobs[key] = js
+	}
+	return jobs, nil
+}
+
+func postMetrics(t *TaskBase, j *work.Job) {
+	if len(j.LastErr) > 0 {
+		_ = redis.HIncr(keyTask(), fieldKey(t, j, hashKeyFail))
+		_ = redis.HSet(keyTask(), fieldKey(t, j, hashKeyErr), truncateString(j.LastErr, 500))
+	}
+}
+
+func truncateString(str string, num int) string {
+	bnoden := str
+	if len(str) > num {
+		if num > 3 {
+			num -= 3
+		}
+		bnoden = str[0:num] + "..."
+	}
+	return bnoden
+}
+
+func fieldKey(t *TaskBase, j *work.Job, field string) string {
+	return fmt.Sprintf("%s:%s:%s", t.GetNamespace(), j.Name, field)
+}
+
+func keyTask() string {
+	return ServicePrefix + keyTasks
+}
---
+++ task/task_daily_report.go
@@ -0,0 +1,145 @@
+package task
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"time"
+
+	"github.com/Guazi-inc/go-work"
+	"github.com/go-errors/errors"
+	"golang.guazi-corp.com/znkf/data-soup/http/staff"
+)
+
+/*******************************************************************************
+ *  脚本名称：TasksDailyReport
+ *  脚本功能：发送每日脚本运行报表
+ *  执行计划：每天凌晨8点
+ *  Author: panxiaohuai
+ *******************************************************************************/
+
+const (
+	timeFormat = "2006-01-02 15:04:05"
+	ns         = "__cron_report__"
+)
+
+// DailyReportTime Global variable, can be modified by application
+var DailyReportTime = "08:00:00"
+var mailGroup map[string]bool
+var redis CronCache
+var staffHTTP *staff.HTTP
+
+// TasksDailyReport job context
+type TasksDailyReport struct {
+	TaskBase
+}
+
+var regexMail = regexp.MustCompile(`^[A-Za-zd]+([-_.][A-Za-zd]+)*@([A-Za-zd]+[-.])+[A-Za-zd]{2,5}$`)
+
+// Setup init func
+func (t *TasksDailyReport) Setup() {
+	t.SetMetaData(t, ns, "panxiaohuai@guazi.com", "每日脚本执行报告", false)
+
+	ts := work.NewSpec()
+	ts.Daily(DailyReportTime)
+	t.AddCronTask("", ts, t.Exec)
+}
+
+// Exec worker func
+func (t *TasksDailyReport) Exec(_ *work.Job) error {
+	jobs, err := fetchMetrics()
+	if err != nil {
+		return err
+	}
+	if staffHTTP == nil {
+		return errors.New("[go-work error] staffHttp nil")
+	}
+
+	return t.sendMail(jobs)
+}
+
+func (t *TasksDailyReport) sendMail(jobs map[string]*jobMetrics) error {
+	body1 := `<h3>智能客服后台Go脚本执行情况报表</h3>
+				<table border="1" cellspacing="0">
+					<tr height=30 bgColor=#00CD66>
+						<td>脚本</td>
+						<td>描述</td>
+						<td>业务线</td>
+						<td>Owner</td>
+						<td>Schedule</td>
+						<td>成功次数</td>
+						<td>失败次数</td>
+						<td>平均耗时(秒)</td>
+						<td>最后报错信息</td>
+					</tr>`
+	for _, v := range jobs {
+		elapsed := 0
+		succ, _ := strconv.Atoi(v.succ)
+		if succ > 0 {
+			total, _ := strconv.Atoi(v.elapsed)
+			elapsed = total / succ
+		}
+
+		body1 += fmt.Sprintf("<tr height=30><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%d</td><td>%s</td></tr>",
+			v.name,
+			v.desc,
+			v.ns,
+			v.owner,
+			v.schedule,
+			v.succ,
+			v.fail,
+			elapsed/1000,
+			v.lastErr,
+		)
+
+	}
+	body1 += "</table>"
+
+	timeNow := time.Now()
+	body := `<!DOCTYPE html><html><head></head><body>` +
+		fmt.Sprintf("统计时间: %s - %s", timeNow.Add(-time.Hour*24).Format(timeFormat), timeNow.Format(timeFormat)) +
+		body1 +
+		"</body></html>"
+
+	url := "contact/sendMail"
+	subject := fmt.Sprintf("智能客服脚本日报 %d-%d-%d", timeNow.Year(), timeNow.Month(), timeNow.Day())
+	var receivers string
+	for r := range mailGroup {
+		receivers += "," + r
+	}
+
+	if len(receivers) == 0 {
+		return nil
+	}
+
+	params := map[string]string{
+		"to":      receivers,
+		"subject": subject,
+		"body":    body,
+	}
+
+	var resp interface{}
+	err := staffHTTP.PostMultipart(url, params, &resp, map[string]string{})
+	if err != nil {
+		panic(err)
+	}
+
+	return nil
+}
+
+func AddMailReceiver(receiver string) {
+	if mailGroup == nil {
+		mailGroup = map[string]bool{}
+	}
+	if regexMail.Match([]byte(receiver)) {
+		mailGroup[receiver] = true
+	}
+}
+
+func SetCache(c CronCache) {
+	redis = c
+}
+
+func SetStaffClient(s *staff.HTTP) {
+	staffHTTP = s
+}
