2e63f490f71638daf86911ace61e56a16e89877a
hallicopter <advait@vernacular.ai>
2020-07-01 16:31:07 +0530

    adds utility of deleting task based on the taskID

--- queue_utils.go
+++ queue_utils.go
@@ -1,7 +1,9 @@
 package work
 
 import (
+	"fmt"
 	"github.com/gomodule/redigo/redis"
+	"strings"
 )
 
 func DeleteQueue(pool *redis.Pool, namespace, jobName string) {
@@ -12,6 +14,25 @@ func DeleteQueue(pool *redis.Pool, namespace, jobName string) {
 	if err != nil {
 		panic("could not delete queue: " + err.Error())
 	}
+}
+
+func DeleteJob(namespace, jobName string, jobId string, pool *redis.Pool) {
+	conn := pool.Get()
+	defer conn.Close()
+	items, err := redis.Values(conn.Do("LRANGE", redisKeyJobs(namespace, jobName), 0, -1))
+	if err != nil {
+		panic("ERROR deleting job! : " + err.Error())
+	}
+
+	for _, item := range items {
+		v, _ := redis.String(item, err)
+		if strings.Contains(v, jobId) {
+			out, _ := conn.Do("LREM", redisKeyJobs(namespace, jobName), 1, v)
+			fmt.Println(out)
+			return
+		}
+	}
+	panic("couldn't find jobID")
 }
 
 func PauseJobs(namespace, jobName string, pool *redis.Pool) error {
