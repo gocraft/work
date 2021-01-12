235b324dd31586efa4e131051bdb33680c0e9807
Austin Arbor <aarbor989@gmail.com>
2021-01-11 23:34:29 -0500

    use guid for job identifier rather than system rand

--- identifier.go
+++ identifier.go
@@ -1,16 +1,9 @@
 package work
 
 import (
-	"crypto/rand"
-	"fmt"
-	"io"
+	"github.com/google/uuid"
 )
 
 func makeIdentifier() string {
-	b := make([]byte, 12)
-	_, err := io.ReadFull(rand.Reader, b)
-	if err != nil {
-		return ""
-	}
-	return fmt.Sprintf("%x", b)
+	return uuid.New().String()
 }
