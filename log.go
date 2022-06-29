e9de327d12db1fc076edc9afd0bbdef0700cdc2b
hageshtrem <hageshtrem@yahoo.com>
2022-06-29 17:47:52 +0300

    Add the ability to configure logger

--- log.go
+++ log.go
@@ -1,7 +1,22 @@
 package work
 
-import "fmt"
+import (
+	"io"
+	"log"
+)
+
+// StdLogger is used to log error messages.
+type StdLogger interface {
+	Print(v ...interface{})
+	Printf(format string, v ...interface{})
+	Println(v ...interface{})
+}
+
+// Logger is the instance of a StdLogger interface that Worker writes connection
+// management events to. By default it is set to discard all log messages via
+// ioutil.Discard, but you can set it to redirect wherever you want.
+var Logger StdLogger = log.New(io.Discard, "[Work] ", log.LstdFlags)
 
 func logError(key string, err error) {
-	fmt.Printf("ERROR: %s - %s\n", key, err.Error())
+	Logger.Printf("Worker error: %s - %s", key, err.Error())
 }
