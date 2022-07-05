ff99a4d92f8fbf3c5ecfd35baff6b705cb49a08d
hageshtrem <hageshtrem@yahoo.com>
2022-07-05 13:23:08 +0300

    Improve message format

--- log.go
+++ log.go
@@ -14,9 +14,9 @@ type StdLogger interface {
 
 // Logger is the instance of a StdLogger interface that Worker writes connection
 // management events to. By default it is set to discard all log messages via
-// ioutil.Discard, but you can set it to redirect wherever you want.
+// io.Discard, but you can set it to redirect wherever you want.
 var Logger StdLogger = log.New(io.Discard, "[Work] ", log.LstdFlags)
 
 func logError(key string, err error) {
-	Logger.Printf("Worker error: %s - %s", key, err.Error())
+	Logger.Printf("key: %s, err: %s", key, err.Error())
 }
