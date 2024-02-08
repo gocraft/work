06d7adf44232567dbb5504171a840a6c5def1b2a
Shahbaz Khan <shahbaz275817@yahoo.com>
2024-02-08 12:32:42 +0530

    fixes and removed irrelevant code

--- cmd/workfakedata/main.go
+++ cmd/workfakedata/main.go
@@ -6,8 +6,8 @@ import (
 	"math/rand"
 	"time"
 
+	"github.com/gocraft/work"
 	"github.com/gomodule/redigo/redis"
-	"github.com/shahbaz275817/work"
 )
 
 var redisHostPort = flag.String("redis", ":6379", "redis hostport")
