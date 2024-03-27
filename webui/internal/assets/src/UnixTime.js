aa7b9949ba913d2f94b1340506b0abcde497414a
gokul1630 <gokulgp3167@gmail.com>
2024-03-27 10:24:06 +0530

    webui: show unix time in locale

--- webui/internal/assets/src/UnixTime.js
+++ webui/internal/assets/src/UnixTime.js
@@ -7,9 +7,14 @@ export default class UnixTime extends React.Component {
   }
 
   render() {
+
+    if(this.props.ts === 0 || this.props.ts === '0' || !this.props.ts){
+      return null;
+    }
+
     let t = new Date(this.props.ts * 1e3);
     return (
-      <time dateTime={t.toISOString()}>{t.toISOString().slice(0, 19).replace(/-/g, '/').replace('T', ' ')}</time>
+      <time dateTime={t.toLocaleString()}>{t.toLocaleString().slice(0, 19).replace(/-/g, '/').replace('T', ' ')}</time>
     );
   }
 }
